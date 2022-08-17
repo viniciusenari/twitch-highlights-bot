eea022129dcf8ba73efc2f7737f0b160967da302
Vinicius Koji Enari <viniciusenari@gmail.com>
2022-08-17 15:36:33 -0300

    feat: Twitch API auth
    Created class to handle authentication with the Twitch API

---
+++ twitch_api.py
@@ -0,0 +1,21 @@
+import requests
+
+class TwitchAPI():
+
+    def __init__(self):
+        self.headers = None
+        pass
+
+    def auth(self, client_id, client_secret):
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }
+        data = f'client_id={client_id}&client_secret={client_secret}&grant_type=client_credentials'
+
+        response = requests.post('https://id.twitch.tv/oauth2/token', headers=headers, data=data)
+        
+        bearer = response.json()['access_token']
+        self.headers = {
+            'Authorization': f'Bearer {bearer}',
+            'Client-Id': client_id,
+        }
