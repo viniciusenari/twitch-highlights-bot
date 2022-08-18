728ae2385549f98df4f0cab410f4446f397d85aa
Vinicius Koji Enari <viniciusenari@gmail.com>
2022-08-18 00:26:32 -0300

    feat: clip downloader
    Download clips from the thumbnail url.

---
+++ clip_downloader.py
@@ -0,0 +1,13 @@
+import requests
+
+class ClipDownloader():
+    def __init__(self):
+        pass
+
+    def download_clip(self, thumb_url, title):
+        index = thumb_url.find('-preview')
+        clip_url = thumb_url[:index] + '.mp4'
+
+        r = requests.get(clip_url)
+        with open(f'{title}.mp4', 'wb') as f:
+            f.write(r.content)
