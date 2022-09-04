84e69e46ff08f833a2b4a8b23c64c100618cfeb5
Vinicius Enari <viniciusenari@gmail.com>
2022-09-03 22:58:48 -0300

    feat: get clips by broadcaster_id and language

--- project/clips.py
+++ project/clips.py
@@ -6,9 +6,10 @@ api = TwitchAPI()
 api.auth(client_id, client_secret)
 
 class ClipContent:
-    def __init__(self, url, broadcaster_id, creator_name, title, thumbnail_url):
+    def __init__(self, url, broadcaster_id, game_id, creator_name, title, thumbnail_url):
         self.url = url
         self.broadcaster_id = broadcaster_id
+        self.game_id = game_id
         self.creator_name = creator_name
         self.title = title
         self.thumbnail_url = thumbnail_url
@@ -20,8 +21,9 @@ class ClipsExtractor:
     def __init__(self):
         self.clips_content = []
 
-    def get_clips(self, game_id, quantity):
+    def get_clips(self, quantity, broadcaster_id = None, game_id = None, languages = []):
         params = {
+            'broadcaster_id' : broadcaster_id,
             'game_id' : game_id,
             'first' : quantity,
             'started_at' : prev_week_sunday,
@@ -31,20 +33,11 @@ class ClipsExtractor:
         response = requests.get('https://api.twitch.tv/helix/clips', params=params, headers=api.headers)
 
         for clip in response.json()['data']:
-            clip_content = ClipContent(url = clip['url'],
-                                        broadcaster_id = clip['broadcaster_id'],
-                                        creator_name = clip['creator_name'],
-                                        title = clip['title'],
-                                        thumbnail_url = clip['thumbnail_url'])
-            self.clips_content.append(clip_content)
-
-
-game_id = games_id['Counter-Strike: Global Offensive']
-
-clip_extractor = ClipsExtractor()
-clip_extractor.get_clips(game_id, 10)
-for clip_content in clip_extractor.clips_content:
-    print(clip_content)
-    print('-------------------------------------------------------------')
-
-
+            if languages == [] or clip['language'] in languages:
+                clip_content = ClipContent(url = clip['url'],
+                                            broadcaster_id = clip['broadcaster_id'],
+                                            game_id = clip['game_id'],
+                                            creator_name = clip['creator_name'],
+                                            title = clip['title'],
+                                            thumbnail_url = clip['thumbnail_url'])
+                self.clips_content.append(clip_content)
