8fb716c7220b608a3e41433dfcb291a2ac74bb82
Vinicius Koji Enari <viniciusenari@gmail.com>
2022-10-03 10:55:22 -0300

    refactor: duration, download_clips, path
    Download clips from clips_extractor, fix issue in which path included '/' if it was on clip title.

--- project/clips.py
+++ project/clips.py
@@ -2,13 +2,13 @@ import requests
 import os
 from twitch_api import TwitchAPI
 from utils import client_id, client_secret, prev_week_saturday, prev_week_sunday
-from games import games_id
+from twitch_ids_box_art import games_id
 
 api = TwitchAPI()
 api.auth(client_id, client_secret)
 
 class ClipContent:
-    def __init__(self, url, broadcaster_id, broadcaster_name, game_id, creator_name, title, thumbnail_url, views, path):
+    def __init__(self, url, broadcaster_id, broadcaster_name, game_id, creator_name, title, thumbnail_url, duration, path):
         self.url = url
         self.broadcaster_id = broadcaster_id
         self.broadcaster_name = broadcaster_name
@@ -16,7 +16,7 @@ class ClipContent:
         self.creator_name = creator_name
         self.title = title
         self.thumbnail_url = thumbnail_url
-        self.views = views
+        self.duration = duration
         self.path = path
     
     def __str__(self):
@@ -25,8 +25,10 @@ class ClipContent:
 class ClipsExtractor:
     def __init__(self):
         self.clips_content = []
+        self.by_game = None
 
     def get_clips(self, quantity = 10, broadcaster_id = None, game_id = None, languages = []):
+        self.by_game = True if game_id else False
         params = {
             'broadcaster_id' : broadcaster_id,
             'game_id' : game_id,
@@ -48,8 +50,8 @@ class ClipsExtractor:
                         clip['creator_name'],
                         clip['title'],
                         clip['thumbnail_url'],
-                        clip['view_count'],
-                        f'files/clips/{clip["title"].replace(" ", "_").lower()}.mp4'
+                        clip['duration'],
+                        f'files/clips/{clip["title"].replace(" ", "_").replace("/","_").lower()}.mp4'
                     ))
             params['after'] = response['pagination']['cursor']
 
@@ -69,10 +71,8 @@ class ClipDownloader():
         else:
             print(f'Failed to download clip from thumb: {clip.thumbnail_url}')
     
-    def download_top_clips(self, quantity, broadcaster_id = None, game_id = None, languages = []):
-        clips_extractor = ClipsExtractor()
-        clips_extractor.get_clips(quantity, broadcaster_id, game_id, languages)
-        for i in range(quantity):
-            print(f'Downloading clip {i+1}/{quantity}')
+    def download_top_clips(self, clips_extractor):
+        for i in range(len(clips_extractor.clips_content)):
+            print(f'Downloading clip {i+1}/{len(clips_extractor.clips_content)}')
             clip = clips_extractor.clips_content[i]
             self.download_clip(clip)
