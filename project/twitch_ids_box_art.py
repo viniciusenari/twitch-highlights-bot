68b2f75e222db919d983b72672dcec2b3f4aa950
Vinicius Koji Enari <viniciusenari@gmail.com>
2022-10-03 10:17:22 -0300

    refactor: renamed file add games_name dict
    Renamed file. Added games_name dictionary to get game name from game id

--- project/twitch_ids_box_art.py
+++ project/twitch_ids_box_art.py
@@ -5,7 +5,7 @@ from utils import client_id, client_secret
 api = TwitchAPI()
 api.auth(client_id, client_secret)
 
-class GetTopGames():
+class GetTopGames:
     def __init__(self):
         pass
 
@@ -15,13 +15,16 @@ class GetTopGames():
         }
 
         games_id = {}
+        games_name = {}
         games_box_art_url = {}
         response = requests.get('https://api.twitch.tv/helix/games/top', params=params, headers=api.headers)
         for game in response.json()['data']:
             games_id[game['name']] = game['id']
+            games_name[game['id']] = game['name']
             games_box_art_url[game['name']] = game['box_art_url'].replace('{width}', '285').replace('{height}', '380')
         
-        return games_id, games_box_art_url
+        return games_id, games_name, games_box_art_url
+
 
 top_games = GetTopGames()
-games_id, games_box_art_url = top_games.get_top_games()
+games_id, games_name, games_box_art_url = top_games.get_top_games()
