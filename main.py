da8781f88c4b7032e52017d8e353be20d95db602
Vinicius Koji Enari <viniciusenari@gmail.com>
2022-12-13 21:33:59 -0300

    feat: add argument parsing

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-from project.cli import CLI
+from project.app import App
+import argparse
 
-if __name__ == "__main__":
-    cli = CLI()
-    cli.run()
+class TwitchVideoCompiler:
+    def __init__(self):
+        pass
+
+    def parse_and_run_args(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-g', '--game', help='Game name', required=True)
+        parser.add_argument('-a', '--amount', help='Amount of clips', required=True)
+        parser.add_argument('-l', '--languages', help='Languages', nargs='+', required=False)
+        args = parser.parse_args()
+
+        app = App()
+        app.run(args.game, int(args.amount), args.languages)
+    
+if __name__ == '__main__':
+    main = TwitchVideoCompiler()
+    main.parse_and_run_args()
