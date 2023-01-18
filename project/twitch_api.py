4a9f09eca11e01cbeb30ec84270d0e887b6ed200
Vinicius Koji Enari <viniciusenari@gmail.com>
2023-01-18 20:03:51 -0300

    feat: add error handling to TwitchAPI class

--- project/twitch_api.py
+++ project/twitch_api.py
@@ -9,11 +9,19 @@ class TwitchAPI():
         headers = {
             'Content-Type': 'application/x-www-form-urlencoded',
         }
+
         data = f'client_id={client_id}&client_secret={client_secret}&grant_type=client_credentials'
 
-        response = requests.post('https://id.twitch.tv/oauth2/token', headers=headers, data=data)
+        try:
+            response = requests.post('https://id.twitch.tv/oauth2/token', headers=headers, data=data)
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as err:
+            raise SystemExit(err)
+        except requests.exceptions.RequestException as err:
+            raise SystemExit(err)
         
         bearer = response.json()['access_token']
+
         self.headers = {
             'Authorization': f'Bearer {bearer}',
             'Client-Id': client_id,
